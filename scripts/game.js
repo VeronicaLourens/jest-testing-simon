37cb67fc42f5708a7e9ab569276ea5a5b94b24b1
Veronica Lourens <Veronica@lourens.cloud>
2022-04-03 08:13:53 +0000

    Add js code and test code.

---
+++ scripts/game.js
@@ -0,0 +1,22 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ['button1', 'button1', 'button3', 'button4']
+}
+
+
+// newGame()
+
+// addTurn()
+
+// showTurns()
+
+// lightsOn()
+
+// playerTurn()
+
+// showScore()
+
+module.exports = {game};
+
